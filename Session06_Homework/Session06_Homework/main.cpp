23a4edcf3cde4e5345da5d64c38d3c30f0a6458d
DanielKopecky6 <43926997+DanielKopecky6@users.noreply.github.com>
2019-11-12 18:05:18 +0100

    Pokus o praci s binarnim souborem.

--- Session06_Homework/Session06_Homework/main.cpp
+++ Session06_Homework/Session06_Homework/main.cpp
@@ -24,6 +24,7 @@ void save() {
 	file.close();
 	delete[] personArray;
 }
+
 void load() {
 	Person* personArray = new Person[ARRAY_SIZE];
 	std::ifstream file;
@@ -36,9 +37,39 @@ void load() {
 	delete[] personArray;
 }
 
+void binarySave() {
+	Person* personArray = new Person[ARRAY_SIZE];
+
+	personArray[0] = Person(std::string("Jan"), std::string("Novak"), Date(1, 1, 2000), Address("Pardubice", "17.Listopadu", 55532));
+	personArray[1] = Person(std::string("Tomas"), std::string("Budina"), Date(2, 2, 1996), Address("Holice", "Ulice", 11111));
+	personArray[2] = Person(std::string("Lukas"), std::string("Zeman"), Date(3, 3, 2001), Address("Mesto1", "Ulice", 12345));
+	personArray[3] = Person(std::string("Jana"), std::string("Novakova"), Date(4, 4, 2002), Address("Mesto2", "Ulice", 67890));
+	personArray[4] = Person(std::string("Martina"), std::string("Konecna"), Date(5, 5, 2003), Address("Mesto3", "Ulice", 54321));
+
+	std::ofstream file;
+	file.open("file.bin", std::ios_base::binary);
+
+		file.write((const char*)&personArray, sizeof(personArray));
+
+	file.close();
+	delete[] personArray;
+}
+
+void binaryLoad() {
+	Person* personArray = new Person[ARRAY_SIZE];
+	std::ifstream file;
+	file.open("file.txt", std::ios_base::binary);
+
+	file.read((char*)personArray, sizeof(personArray));
+
+	delete[] personArray;
+}
+
 int main(void) {
 	save();
 	load();
+	//binarySave();
+	//binaryLoad();
 
 	std::cin.get();
 	return 0;
